44abec883176b9974c817e59d4ea93aa59ac0a2b
Ishita Jain <59766680+ishita303@users.noreply.github.com>
2020-08-30 14:40:13 +0530

    new

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
+app.use(cors());
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(path.join(publicPath, 'client/index.html'));
 });
 
 app.listen(port, () => {
